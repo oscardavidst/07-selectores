966a43e9ce0350307000b15f0c85957dd621df1a
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-25 16:54:38 -0500

    Configuracion de rutas y componentes con LazyLoad

---
+++ src/app/paises/pages/selector/selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-selector',
+  templateUrl: './selector.component.html',
+  styles: [
+  ]
+})
+export class SelectorComponent {
+
+}
