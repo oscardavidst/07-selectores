8e022ffbfeeeb24c9cbc4a5a5ea81a7007d5e8ba
Oscar Soto <oscarqpzm95@gmail.com>
2023-03-26 01:06:25 -0500

    Bloquear selects

--- src/app/paises/pages/selector/selector.component.ts
+++ src/app/paises/pages/selector/selector.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Fronteras, PaisSmall } from '../../interfaces/pais.interface';
 import { PaisesService } from '../../services/paises.service';
-import { switchMap, tap } from 'rxjs';
+import { delay, switchMap, tap } from 'rxjs';
 
 @Component({
   selector: 'app-selector',
@@ -10,6 +10,7 @@ import { switchMap, tap } from 'rxjs';
   styles: [],
 })
 export class SelectorComponent implements OnInit {
+  loading: boolean = false;
   miFormulario: FormGroup = this.formBuilder.group({
     continente: ['', [Validators.required]],
     pais: ['', [Validators.required]],
@@ -32,23 +33,35 @@ export class SelectorComponent implements OnInit {
     this.miFormulario
       .get('continente')
       ?.valueChanges.pipe(
+        tap((_) => {
+          this.miFormulario.get('pais')?.reset('');
+          this.loading = true;
+        }),
         switchMap((valorContinente) =>
           this.paisesService.getPaisesByContinente(valorContinente)
-        ),
-        tap((_) => this.miFormulario.get('pais')?.reset(''))
+        )
       )
-      .subscribe((respPaises) => (this.paises = respPaises));
+      .subscribe((respPaises) => {
+        this.paises = respPaises;
+        this.loading = false;
+      });
 
     // Cuando cambia el pais
     this.miFormulario
       .get('pais')
       ?.valueChanges.pipe(
-        switchMap((codigo) => this.paisesService.getFronterasByCode(codigo)),
-        tap((_) => this.miFormulario.get('frontera')?.reset(''))
+        tap((_) => {
+          this.fronteras = [];
+          this.miFormulario.get('frontera')?.reset('');
+          this.loading = true;
+        }),
+        delay(2000),
+        switchMap((codigo) => this.paisesService.getFronterasByCode(codigo))
       )
-      .subscribe(
-        (fronteras) => (this.fronteras = fronteras ? fronteras.borders : [])
-      );
+      .subscribe((fronteras) => {
+        this.fronteras = fronteras ? fronteras.borders : [];
+        this.loading = false;
+      });
   }
 
   guardar() {
